759e7d51a880905734eb64bbeaf96fc8119fdb43
Jakub <jakub.jarcza@gmail.com>
2018-05-14 15:41:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/jakub/lab4/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.jakub.lab4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] values = new String[] {"Pies", "Kot", "Kon", "Golab", "Kruk", "Dzik", "Karp", "Osiol", "Chomik", "Mysz", "Jez", "Karaluch"};
+        this.target = new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+        this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
+        ListView listView= (ListView) findViewById(R.id.listView);
+        listView.setAdapter(this.adapter);
+
+    }
+}
